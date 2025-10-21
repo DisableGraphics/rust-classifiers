9ca25461af143d998f6c8fe0e64c0b85c00ad918
DisableGraphics <elchifladod@gmail.com>
2025-10-21 13:30:07 +0200

    a bit more descriptive

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use linfa_datasets::{iris, winequality};
 use crate::eucdist::EuclideanDistanceClassifier;
 
 trait Classifier {
-	fn fit(&mut self, data: ArrayView2<f64>, labels: ArrayView1<usize>);
+	fn fit(&mut self, data: ArrayView2<f64>, targets: ArrayView1<usize>);
 	fn decision_function(&self, data: ArrayView2<f64>) -> Array2<f64>;
 	fn predict(&self, data: ArrayView2<f64>) -> Array1<usize>;
-	fn score(&self, data: ArrayView2<f64>, labels: ArrayView1<usize>) -> f64 {
+	fn score(&self, data: ArrayView2<f64>, targets: ArrayView1<usize>) -> f64 {
 		let preds = self.predict(data);
-		assert_eq!(preds.len(), labels.len(), "Vectors must have the same length");
+		assert_eq!(preds.len(), targets.len(), "Vectors must have the same length");
 		let correct = preds.iter()
-			.zip(labels.iter())
+			.zip(targets.iter())
 			.filter(|(pred, true_val)| **pred as usize == **true_val)
 			.count();
 
@@ -34,11 +34,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 		for (datasetname, dataset) in datasets.iter() {
 			let records = dataset.records.clone().into_dimensionality()?;
 			let records = records.view();
-			let labels = dataset.targets.clone().into_dimensionality()?;
-			let labels = labels.view();
+			let targets = dataset.targets.clone().into_dimensionality()?;
+			let targets = targets.view();
 			classifier
-				.fit(records, labels);
-			println!("Score for dataset {}: {}", datasetname, classifier.score(records, labels))
+				.fit(records, targets);
+			println!("Score for dataset {}: {}", datasetname, classifier.score(records, targets))
 		}
 	}
 	Ok(())
